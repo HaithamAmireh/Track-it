2814205f36725bf712c2ad6616908450c17f3413
haithamamireh <haithamamireh@gmail.com>
2023-01-20 15:10:21 +0300

    created backend with two tables using sqlalchemy

---
+++ backend/main.py
@@ -0,0 +1,94 @@
+from flask import Flask, request
+from flask_restx import Api, Resource, fields
+from config import DevConfig
+from models import ExpenseEntry
+from exts import db
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+app.config.from_object(DevConfig)
+
+db.init_app(app)
+
+migrate = Migrate(app, db)
+
+api = Api(app,doc='/docs')
+
+#model (serializer) so we can get the db entries as json
+entry_model = api.model(
+    "Entry",
+    {
+        "id": fields.Integer(),
+        "amount": fields.Integer(),
+        "title": fields.String(),
+        "category": fields.String()
+    }
+)
+
+@api.route('/hello')
+class HelloResource(Resource):
+    def get(self):
+        return {"message": "Hello world"}
+
+
+@api.route('/entries')
+class EntriesResource(Resource):
+    @api.marshal_list_with(entry_model)
+    def get(self):
+        """Get all entries"""
+        entries = ExpenseEntry.query.all()
+
+        return entries, 201
+
+    @api.marshal_with(entry_model)
+    def post(self):
+        """create an entry"""
+        data = request.get_json()
+
+        new_entry = ExpenseEntry(
+            title = data.get('title'),
+            amount = data.get('amount'),
+            category = data.get('category')
+        )
+
+        new_entry.save()
+        
+        return new_entry
+
+@api.route('/entry/<int:id>')
+class EntryResource(Resource):
+    @api.marshal_with(entry_model)
+    def get(self, id):
+        """Get a single entry"""
+        entry = ExpenseEntry.query.get_or_404(id)
+        return entry
+
+    @api.marshal_with(entry_model)
+    def put(self, id):
+        """updatea an entry"""
+        entry_to_update = ExpenseEntry.query.get_or_404(id)
+        data = request.get_json()
+        entry_to_update.update(data.get('amount'), data.get('title'), data.get('category'))
+
+        return entry_to_update
+
+    @api.marshal_with(entry_model)
+    def delete(self, id):
+        "delete an entry"
+        entry_to_delete = ExpenseEntry.query.get_or_404(id)
+        entry_to_delete.delete()
+        return entry_to_delete
+
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        "db": db,
+        "Entry": ExpenseEntry
+    }
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ backend/models.py
@@ -0,0 +1,64 @@
+from exts import db
+
+
+"""
+class ExpenseEntry
+    id:int primary key
+    amount: int
+    title: str
+    category: str
+"""
+
+class ExpenseEntry(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    title = db.Column(db.String(), nullable=False)
+    amount = db.Column(db.Integer(), nullable=True)
+    category = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f"<Entry {self.title}>"
+
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+    def update(self,amount, title, category):
+        self.amount = amount
+        self.title = title
+        self.category = category
+        db.session.commit()
+
+
+"""
+class User:
+    id: int primary_key
+    username : str
+    email : String
+    password : string text
+"""
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), nullable=False, unique=True)
+    email = db.Column(db.String(80), nullable=False)
+    password = db.Column(db.Text(), nullable=False)
+    def __repr__(self):
+        return f"<User {self.username}>"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
